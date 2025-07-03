1d481baa349d68be266c401290bc83366732bf46
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-07-03 17:47:54 +0900

    (2025_07_03) 게임 UI 색상 클래스 구현하기

--- Ch15/main.cpp
+++ Ch15/main.cpp
@@ -177,7 +177,12 @@ public:
 		return maxWeight >= currentWeight + addItem.GetWeight();
 	}
 
-	int findBestItem(int targetWeight, std::vector<ItemW>& selectableItems)
+	/*
+	*  1. 선택한 아이템 들 중에서 가장 큰 가치, 실제로 선택된 아이템들 (자료구조)
+	*  -> 반환값을 pair< T1 , T2 >
+	*/
+
+	 std::pair<int,std::vector<ItemW>> findBestItem(int targetWeight, std::vector<ItemW>& selectableItems)
 	{
 		 // 현재 아이템의 무게에 새로운 아이템 조합을 가져올 때 저장할 컨테이너 선언
 
@@ -186,6 +191,7 @@ public:
 		// selectableItems 들어있는 무게. weight
 		// selectableitems 들어있는 가치. value
 
+		std::vector<std::vector<bool>> selected(selectableItems.size(), std::vector<bool>(targetWeight + 1, false));
 
 		for (int i = 0; i < selectableItems.size(); i++)
 		{
@@ -194,19 +200,34 @@ public:
 
 			for (int w = targetWeight; w >= weight; w--)
 			{
-
 				/*
 				*  해당 코드를 위의 알고리즘 코드로 작성해보세요.
-				*/
-
+				*/				
+				if (dp[w - weight] + value > dp[w])  // 새로들어온 아이템 + 기존 무게  전체 무게 보다 클 때만
+				{
+					dp[w] = dp[w - weight] + value;
+					selected[i][w] = true;
+				}
+				
 			}
 		}
 
 		// 계산한 최적의 조합을 역산해서 다시 vector 저장하는 코드.
 		// while(weight - 무게) 
 
+		std::vector<ItemW> bestItems;
+		int w = targetWeight;
 
-		return dp[targetWeight];
+		for (int i = selectableItems.size() - 1; i >= 0 && w > 0; i--)
+		{
+			if (selected[i][w])
+			{
+				bestItems.push_back(selectableItems[i]);
+				w -= selectableItems[i].GetWeight();
+			}
+		}
+
+		return std::make_pair(dp[targetWeight], bestItems);
 	}
 
 
@@ -228,10 +249,30 @@ void InventoryWeightSystem()
 	ItemW D("D", 5, 12);
 
 	std::vector<ItemW> selectableTable{ A,B,C,D };
-	std::cout << "주어진 아이템의 최대 가치 : " << inventory.findBestItem(7, selectableTable) << std::endl;
+	
+	std::pair<int, std::vector<ItemW>> bestitems = inventory.findBestItem(7, selectableTable);
 
-	inventory.AddItem(A);
-	inventory.AddItem(B);
+	std::cout << "주어진 아이템의 최대 가치 :" << bestitems.first << std::endl;	
+	std::vector<ItemW> ItemC = bestitems.second;
+
+	int i = 0;
+
+	// 인벤토리에 아이템을 넣는 코드를 최선의 아이템을 인벤토리에 추가하도록 수정해보세요.
+	//inventory.AddItem(A);
+	//inventory.AddItem(B);
+	for (auto& item : ItemC)
+	{
+		i++;
+		std::cout << i << "번째 아이템의 이름 : " << item.GetName() << ", 무게 : " << item.GetWeight() << ", 가치 : " << item.GetValue() << std::endl;
+		// 유저가 허락했을 때 실행하세요 if( 유저 입력 코드)
+		inventory.AddItem(item);
+	}
+
+	//std::cout << "주어진 아이템의 최대 가치 : " << 
+	//	
+	//inventory.findBestItem(7, selectableTable) << std::endl;
+
+
 
 	int index = 0;
 
